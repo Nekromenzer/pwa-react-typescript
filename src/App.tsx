0da441e69b6491893dc661a768ec9921a593008c
Maneesha Gimshan <bamaneeshagimshan1999@gmail.com>
2024-03-22 13:21:39 +0530

    load lazy

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,33 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
+import React, { lazy, Suspense } from "react";
+
+const Home = lazy(() => import("./pages/Home"));
+const About = lazy(() => import("./pages/About"));
+
+const SuspenseWrapper = ({ children }: any) => (
+  <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>
+);
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
+      <Route
+        path="/"
+        element={
+          <SuspenseWrapper>
+            <Home />
+          </SuspenseWrapper>
+        }
+      />
+      <Route
+        path="/about"
+        element={
+          <SuspenseWrapper>
+            <About />
+          </SuspenseWrapper>
+        }
+      />
     </Routes>
   );
 }
