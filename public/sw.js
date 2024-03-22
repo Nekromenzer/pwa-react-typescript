a86baa783c5e6f5d171f4e6d9598d651f44c2071
Maneesha Gimshan <bamaneeshagimshan1999@gmail.com>
2024-03-22 17:44:12 +0530

    offline pwa done

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,27 @@
 console.log("Service Worker Loaded");
+let cacheData = "appV1";
+
+this.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(cacheData).then((cache) => {
+      cache.addAll([
+        "/static/js/main.chunk.js",
+        "/static/js/0.chunk.js",
+        "/static/js/bundle.js",
+        "static/js/src_pages_Home_tsx.chunk.js",
+        "/",
+        "/index.html",
+      ]);
+    })
+  );
+});
+
+this.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((resp) => {
+      if (resp) {
+        return resp;
+      }
+    })
+  );
+});
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { lazy, Suspense } from "react";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
 
-const Home = lazy(() => import("./pages/Home"));
-const About = lazy(() => import("./pages/About"));
+// const Home = lazy(() => import("./pages/Home"));
+// const About = lazy(() => import("./pages/About"));
 
 const SuspenseWrapper = ({ children }: any) => (
   <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>
