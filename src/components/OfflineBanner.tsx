ed8d15f219fa4a48c4679d14b59922f37d671a42
Maneesha Gimshan <bamaneeshagimshan1999@gmail.com>
2024-03-25 16:02:45 +0530

    offline banner

---
+++ src/components/OfflineBanner.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Offline = () => {
+  return (
+    <div
+      style={{
+        color: "red",
+        fontSize: "2rem",
+        textAlign: "center",
+        marginTop: "2rem",
+        width: "100%",
+        height: "4rem",
+        background: "rgba(255, 0, 0, 0.1)",
+      }}
+    >
+      Offline
+    </div>
+  );
+};
+
+export default Offline;
