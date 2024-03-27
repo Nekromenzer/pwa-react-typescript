75f30bbe36e76e41cfbacfad81923166d0866a29
Maneesha Gimshan <bamaneeshagimshan1999@gmail.com>
2024-03-27 17:53:09 +0530

    sample form

--- src/pages/Forms.tsx
+++ src/pages/Forms.tsx
@@ -2,7 +2,43 @@ import React from "react";
 import Wrapper from "../components/Wrapper";
 
 const Forms = () => {
-  return <Wrapper header="Forms">Forms</Wrapper>;
+  const [data, setData] = React.useState({ name: "", email: "", message: "" });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = () => {
+    console.log(data);
+  };
+  return (
+    <Wrapper header="Here we demonstrate form action with tanstack online/offline">
+      <br />
+      <div>
+        <label htmlFor="name">Name:</label>
+        <br />
+        <input type="text" id="name" name="name" onChange={handleChange} />
+        <br />
+        <label htmlFor="email">Email:</label>
+        <br />
+        <input type="email" id="email" name="email" onChange={handleChange} />
+        <br />
+        <label htmlFor="message">Message:</label>
+        <br />
+        <input
+          type="text"
+          id="message"
+          name="message"
+          onChange={handleChange}
+        />
+        <br />
+        <br />
+        <button type="button" onClick={handleSubmit}>
+          Submit
+        </button>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default Forms;
